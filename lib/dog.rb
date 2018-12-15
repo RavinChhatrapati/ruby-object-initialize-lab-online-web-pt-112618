42f51158f302abdc50574575ddb486ede9e2a84b
Ravin Chhatrapati <ravinchhatrapati95@gmail.com>
2018-12-15 02:45:39 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,14 @@
 class Dog
-def initialize(name, breed = "Mutt")
-  @name = name
+def initialize(name, breed = "Mutt")  # sets Mutt as a default value
+  @name = name                        # initializes name and breed as specific attributes of each object
   @breed = breed
 
 end
-def name=(name)
+def name=(name)                       
   @name = name
 end
 def breed=(breed)
 @breed = breed
-
 end
 def name
 @name
